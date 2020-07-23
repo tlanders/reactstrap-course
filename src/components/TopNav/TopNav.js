c043c1d7e1011c5027058816f854d49f687353a6
tlanders <tlanders@users.noreply.github.com>
2020-07-23 16:44:18 -0500

    added axios started TopNav component

---
+++ src/components/TopNav/TopNav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './TopNav.css';
+
+class TopNav extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <nav>This is the nav bar</nav>
+        );
+    }
+}
+
+export default TopNav;
