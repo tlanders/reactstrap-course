3be8fd4369a92cd0eb173ae0697fb8667664e49a
tlanders <768713+tlanders@users.noreply.github.com>
2020-07-28 21:15:31 -0500

    started work on DealerLocator component

--- src/App.js
+++ src/App.js
@@ -5,45 +5,47 @@ import TopNav from "./components/TopNav";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import {BrowserRouter as Router, Route} from "react-router-dom";
+import DealerLocator from "./components/DealerLocator";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      vehicleData: null
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            vehicleData: null
+        };
+    }
 
-  componentDidMount() {
-    Axios.get('http://localhost:3001/vehicles')
-        .then(res => {
-          console.log(res.data);
-          this.setState({
-            vehicleData: res.data
-          });
-        })
-        .catch(err => console.log(err));
-  }
+    componentDidMount() {
+        Axios.get('http://localhost:3001/vehicles')
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+                    vehicleData: res.data
+                });
+            })
+            .catch(err => console.log(err));
+    }
 
-  render() {
-      if(this.state.vehicleData) {
-          return (
-              <Router>
-                  <div className="App">
-                      <TopNav vehicleData={this.state.vehicleData}/>
-                      <div className={"contentArea"}>
-                        <Route
-                            exact path={'/'}
-                            render={(props) => <Home {...props} vehicleData={this.state.vehicleData}/>} />
-                      </div>
-                      <Footer/>
-                  </div>
-              </Router>
-          );
-      } else {
-          return (<div className={"App"}><h4><i className="fas fa-spinner fa-spin"></i> Loading...</h4></div>);
-      }
-  }
+    render() {
+        if(this.state.vehicleData) {
+            return (
+                <Router>
+                    <div className="App">
+                        <TopNav vehicleData={this.state.vehicleData}/>
+                        <div className={"contentArea"}>
+                            <Route
+                                exact path={'/'}
+                                render={(props) => <Home {...props} vehicleData={this.state.vehicleData}/>}/>
+                            <Route path={'/find-a-dealer'} component={DealerLocator}/>
+                        </div>
+                        <Footer/>
+                    </div>
+                </Router>
+            );
+        } else {
+            return (<div className={"App"}><h4><i className="fas fa-spinner fa-spin"></i> Loading...</h4></div>);
+        }
+    }
 }
 
 export default App;
